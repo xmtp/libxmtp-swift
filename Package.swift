33ddaf1d8026b3225e0895181f62b570480a01e3
Pat Nakajima <patnakajima@gmail.com>
2023-08-28 13:10:14 -0700

    fix conflicts

--- Package.swift
+++ Package.swift
@@ -1,34 +1,9 @@
-<<<<<<< Updated upstream
-// swift-tools-version:5.9
-=======
-import Foundation
-// swift-tools-version:5.3
->>>>>>> Stashed changes
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-<<<<<<< Updated upstream
-        name: "LibXMTP",
-        platforms: [
-            .iOS(.v13), 
-            .macOS(.v11)
-        ],
-        products: [
-            .library(
-                name: "LibXMTP",
-                targets: ["LibXMTP", "LibXMTPRust"]),
-        ],
-        targets: [
-            .target(
-                name: "LibXMTP",
-                dependencies: ["LibXMTPRust"],
-                path: "Sources/LibXMTP"
-            ),
-            .binaryTarget(
-                name: "LibXMTPRust",
-                path: "./LibXMTPRust.xcframework")
-        ]
-=======
     name: "LibXMTP",
     platforms: [
         .iOS(.v13),
@@ -52,5 +27,4 @@ let package = Package(
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
->>>>>>> Stashed changes
 )
