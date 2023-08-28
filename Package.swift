5f89f12640e6564536fe510efb31c447598ac3e8
Pat Nakajima <patnakajima@gmail.com>
2023-08-28 13:07:11 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 // swift-tools-version:5.9
-import PackageDescription
+=======
 import Foundation
+// swift-tools-version:5.3
+>>>>>>> Stashed changes
+import PackageDescription
 
 let package = Package(
+<<<<<<< Updated upstream
         name: "LibXMTP",
         platforms: [
             .iOS(.v13), 
@@ -23,4 +28,29 @@ let package = Package(
                 name: "LibXMTPRust",
                 path: "./LibXMTPRust.xcframework")
         ]
+=======
+    name: "LibXMTP",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "LibXMTP",
+            targets: ["LibXMTP", "LibXMTPRust"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "LibXMTP",
+            dependencies: ["LibXMTPRust"],
+            path: "Sources/LibXMTP"
+        ),
+        .binaryTarget(
+            name: "LibXMTPRust",
+            path: "LibXMTPRust.xcframework"
+        ),
+        .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
+    ]
+>>>>>>> Stashed changes
 )
