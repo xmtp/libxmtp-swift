22a0f438c244cd9036814ca4adf05292dbbc1df0
Naomi Plasterer <naomi@xmtp.com>
2024-07-18 16:37:32 -0600

    bump version to fix logging issue

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-11a51ca/LibXMTPSwiftFFI.zip",
-            checksum: "dc1d100a8b29580e7e8647c58d8fb69a4e961c2a96d60572307a17d764d500eb"
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-2de056e/LibXMTPSwiftFFI.zip",
+            checksum: "f66556c05d27936bc5a9b9d5ba997fd8fb61299697c809d3030df5f9667ad96c"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
