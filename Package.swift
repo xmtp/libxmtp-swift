d5ade5c5b44fc9a556625320b16b9181ff88cb6c
Naomi Plasterer <naomi@xmtp.com>
2024-07-19 15:08:49 -0600

    remove the cargo changes

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-6b6cf71/LibXMTPSwiftFFI.zip",
-            checksum: "92ca10e455571ff99b3c392a48c3ad69e259f32bf59ed4225b25138c07cc736f"
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-042c83b/LibXMTPSwiftFFI.zip",
+            checksum: "e1ed8777821debe5da398fdc67f96963da7f417f42709caed08cb2c670d1f9a1"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
