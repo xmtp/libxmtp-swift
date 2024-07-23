a4e11394db8078331e3d7ac66d9b11f4c045c3f2
cameronvoell <cameronvoell@users.noreply.github.com>
2024-07-23 08:35:25 -0700

    update libxmtp for custom permissions on group create

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-9698552/LibXMTPSwiftFFI.zip",
-            checksum: "114b0d8d2787b4f24bc89f977bc08fc4395a7f6d6b7cc7d9dcd0318bd0891b8f"
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-4a572e8/LibXMTPSwiftFFI.zip",
+            checksum: "92be1828b74f8c8d3d9fcac65f2ec694b626fc5cf979b55c196ca4cab9d22c4b"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
