1e8e741fc9eaa95bee07e61a7e8d4493815539d6
Naomi Plasterer <naomi@xmtp.com>
2024-11-07 12:34:10 -0800

    update to latest libxmtp with sync conset

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-fd34e68/LibXMTPSwiftFFI.zip",
-            checksum: "e71c460f4de55fe34c8a91a618fdb9cb6db3ac342ac1ea0a6fcea7e43fd51cb6"
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-0d3bf35/LibXMTPSwiftFFI.zip",
+            checksum: "594f8c5923ce8071071e2a885544d5fcd57c2655963edea636212492be144ed7"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
