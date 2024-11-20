f0a019d1ea2c07c455f55f411d3d4285101da4d0
Naomi Plasterer <naomi@xmtp.com>
2024-11-20 15:50:27 -0800

    bump the pod spec

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-46e3bc7/LibXMTPSwiftFFI.zip",
-            checksum: "40a4f9352f0ee6af9cd194625f3a7c85b56c89e0955ee824c0cbcb81adce5242"
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-9d7aa5d/LibXMTPSwiftFFI.zip",
+            checksum: "94933db15aac507d7b750a36d86311680de2095479f340a07f83b4f6024fb1b4"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
