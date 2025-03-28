b521dc16455ae55e7bb764a064167a0e3d749acc
cameronvoell <cameronvoell@users.noreply.github.com>
2025-03-28 12:38:57 -0700

    update bindings for hot fix 2

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LibXMTPSwiftFFI",
-            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-d49c8a3/LibXMTPSwiftFFI.zip",
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-28b8435/LibXMTPSwiftFFI.zip",
             checksum: "4c6f69877004e193ad1fe41c2a83069c6640c10f0ba0eefc2df36314c1d61937"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
