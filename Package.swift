5ab18860c10db279b35e6ac3864d5cf3600d465a
Andrew Plaza <github@andrewplaza.dev>
2025-10-22 12:22:03 -0400

    dynamic swift in Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,14 @@ let package = Package(
     products: [
         .library(
             name: "LibXMTP",
+            type: .static,
             targets: ["LibXMTP", "LibXMTPSwiftFFI"]
         ),
+        .library(
+            name: "LibXMTP"
+            type: .dynamic
+            targets: ["LibXMTPDynamic"]
+        )
     ],
     targets: [
         .target(
@@ -29,6 +35,20 @@ let package = Package(
             name: "LibXMTPSwiftFFI",
             url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.4.0.a9d19aa/LibXMTPSwiftFFI.zip",
             checksum: "a4bcf78ced5f4dd80c161a17a498bac508a30b59f72dfba9c5318020528ccc0e"
+        ),
+        .target(
+            name: "LibXMTPDynamic",
+            dependencies: ["LibXMTPSwiftFFIDynamic"],
+            path: "Sources/LibXMTP",
+            linkerSettings: [
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("SystemConfiguration")
+            ]
+        ),
+        .binaryTarget(
+            name: "LibXMTPSwiftFFIDynamic",
+            url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.4.0.a9d19aa/LibXMTPSwiftFFIDynamic.zip",
+            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
         ),
         .testTarget(name: "LibXMTPTests", dependencies: ["LibXMTP"]),
     ]
